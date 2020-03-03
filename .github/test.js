c5308d45e416ba9a9a6324dbf68afe2fe31df640
Dorian Eydoux <dorian.eydoux@icloud.com>
2020-03-03 20:17:01 +0100

    Update test.js

--- .github/test.js
+++ .github/test.js
@@ -1,5 +1,5 @@
 require("../index");
 console.info("The index file can run");
 
-Object.keys(require("./package.json").dependencies).forEach(dependence => require(dependence));
+Object.keys(require("../package.json").dependencies).forEach(dependence => require(dependence));
 console.info("All dependencies can be load");
